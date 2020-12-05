dabc15b338721905c7ca845b95315c9a4d3a67d3
Evgeniya Kotova <zheniyasl@gmail.com>
2020-12-05 15:02:32 +0100

    change config vars and requirements.txt

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,4 +2,4 @@
 export AUTH0_DOMAIN = 'easylist.eu.auth0.com'
 export API_AUDIENCE = "recipe"
 export ALGORITHMS = ["RS256"]
-export DATABASE_URL="postgres://localhost:5432/easylist"
+export DATABASE_URL="postgres://iqzchqzowufyqm:fcbba5215d9d7770962ddffa8ff2d054eb629400e013081717d1d492f6c0f724@ec2-52-5-176-53.compute-1.amazonaws.com:5432/d4t5krmjvo48qv"
